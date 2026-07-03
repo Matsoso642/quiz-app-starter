735d066f624ea3e4afe08ab89bb6231da19ad161
Matsoso <matsosorelebohile78@gmail.com>
2026-07-03 09:12:30 +0200

    Removed stats and revies.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,49 @@
-import {useState} from "react";
+import { useState } from "react";
 
-function Navbar(){
+function Navbar() {
+  const [open, setOpen] = useState(false);
 
-    const[open, setOpen] = useState(false);
-
-    return(
-        <>
-        <button
+  return (
+    <>
+      <button
         className="btn btn-dark d-md-none sidebar-toggle"
         onClick={() => setOpen(true)}
         aria-label="Open menu"
-        >
+      >
         <i className="bi bi-list" />
-        </button>
+      </button>
 
-        {open && <div className="sidebar-backdrop d-md-none" onClick={() => setOpen(false)} />}
+      {open && (
+        <div
+          className="sidebar-backdrop d-md-none"
+          onClick={() => setOpen(false)}
+        />
+      )}
 
-        <aside className={`sidebar ${open ? "sidebar-open" : ""}`}>
-            <div className="sidebar-header">
-            <h2>Team Beta</h2>
-             <button className="btn-close-custom d-md-none" onClick={() => setOpen(false)} aria-label="Close menu">
-                        <i className="bi bi-x-lg" />
-                    </button>
-                </div>
+      <aside className={`sidebar ${open ? "sidebar-open" : ""}`}>
+        <div className="sidebar-header">
+          <h2>Team Beta</h2>
+          <button
+            className="btn-close-custom d-md-none"
+            onClick={() => setOpen(false)}
+            aria-label="Close menu"
+          >
+            <i className="bi bi-x-lg" />
+          </button>
+        </div>
 
-
-            <nav>
-                    <ul>
-                        <li><a href="#"><i className="bi bi-plus-circle" /> New Quiz</a></li>
-                        <li><a href="#"><i className="bi bi-bar-chart" /> Stats</a></li>
-                        <li><a href="#"><i className="bi bi-eye" /> Review</a></li>
-                    </ul>
-                </nav>
-            </aside>
-        </>
-    )
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                <i className="bi bi-plus-circle" /> New Quiz
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
+  );
 }
 
 export default Navbar;
